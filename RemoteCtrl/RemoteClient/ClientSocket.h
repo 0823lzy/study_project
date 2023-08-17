cecf100cf969f9a58cc8b7108acaa1bf0ad6bae3
liziyuan <387850511@qq.com>
2023-08-17 10:23:00 +0800

    1 解决文件夹中显示的bug(会缺漏文件)

--- RemoteCtrl/RemoteClient/ClientSocket.h
+++ RemoteCtrl/RemoteClient/ClientSocket.h
@@ -161,18 +161,17 @@ public:
 	int  DealCommand() {
 		if (m_sock == -1)return -1;
 		char* buffer = m_buffer.data();
-		memset(buffer, 0, BUFFER_SIZE);
-		size_t index = 0;
+		static size_t index = 0;
 		while (true) {
 			size_t len = recv(m_sock, buffer + index, BUFFER_SIZE - (int)index, 0);
-			if (len <= 0) {
+			if ((len <= 0) && (index==0)) {
 				return -1;
 			}
 			index += len;
 			len = index;
 			m_packet = CPacket((BYTE*)buffer, len);
 			if (len > 0) {
-				memmove(buffer, buffer + len, BUFFER_SIZE - len);
+				memmove(buffer, buffer + len, index - len);
 				index -= len;
 				return m_packet.sCmd;
 			}
@@ -223,6 +222,7 @@ private:
 			exit(0);
 		}
 		m_buffer.resize(BUFFER_SIZE);
+		memset(m_buffer.data(), 0, BUFFER_SIZE);
 	}
 	~CClientSocket() {
 		closesocket(m_sock);
